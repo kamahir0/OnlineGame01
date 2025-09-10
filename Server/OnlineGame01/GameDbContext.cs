ddf41c4888da087b6b470907a6842dd538cb768c
kamahir0 <hrtk1330@gmail.com>
2025-09-10 23:57:52 +0900

    DB

---
+++ Server/OnlineGame01/GameDbContext.cs
@@ -0,0 +1,20 @@
+﻿namespace OnlineGame01;
+
+// GameDbContext.cs
+using Microsoft.EntityFrameworkCore;
+
+public class GameDbContext : DbContext
+{
+    // 1. PlayerScoreを保存するためのテーブルが欲しい、とEF Coreに伝えます
+    public DbSet<PlayerScore> Scores { get; set; }
+
+    // 2. データベースのファイル名を指定します
+    private const string DatabaseName = "ranking.db";
+
+    // 3. このDbContextがどうやってデータベースに接続するかを設定します
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // "ranking.db" という名前のSQLiteファイルを使うように設定
+        optionsBuilder.UseSqlite($"Data Source={DatabaseName}");
+    }
+}
